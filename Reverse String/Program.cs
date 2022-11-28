e6e970c4e299f2e63d6d6295877141e7008874bc
Nimap <Nimap@DESKTOP-22H8R70>
2022-11-28 15:30:32 +0530

    Add project files.

---
+++ Reverse String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            string revString = " ";
+            Console.WriteLine("Enter The String Values: ");
+            str=Console.ReadLine();
+            str.ToLower();
+            for(int i =1; i<str.Length; i++)
+            {
+                revString = revString + str[i];
+                Console.WriteLine("The Reverse String Is: "+revString);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ While Loop - (1 To 100)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop____1_To_100_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while(i<=100)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
