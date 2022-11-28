b3bcf40c1943672138ff7e12f0c6cea899e1e112
Nimap <Nimap@DESKTOP-22H8R70>
2022-11-28 15:51:27 +0530

    added factorial number program

---
+++ Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int fact = 1;
+            //int num = 5;
+            int num;
+            Console.WriteLine("Enter The Number: ");
+            num = Convert.ToInt32(Console.ReadLine());  
+
+
+            for(i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+            Console.ReadLine();
+        }
+    }
+}
