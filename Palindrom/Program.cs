2ca50c3fb010b47e5163c556f93727d1f363e0e5
Nimap <Nimap@DESKTOP-22H8R70>
2022-11-28 15:40:49 +0530

    Added palindrom program

---
+++ Palindrom/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int number = 141;
+          
+            int number;
+            Console.WriteLine("Enter The Number: ");
+            number= Convert.ToInt32(Console.ReadLine());
+
+            int result = 0;
+            int rem;
+            int temp = number;
+
+            while(number!=0)
+            {
+                rem = number % 10;
+                result = result * 10 + rem;
+                number = number / 10;
+            }
+
+            number = temp;
+            if(number == result)
+            {
+                Console.WriteLine("This is Palindrom!!");
+            }
+            else
+            {
+                Console.WriteLine("This is Not Palindrom!!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
