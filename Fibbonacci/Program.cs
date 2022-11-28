bcb06702bb2b912748a716688a37c9d53047c232
Nimap <Nimap@DESKTOP-22H8R70>
2022-11-28 16:08:55 +0530

    added Fabbonacci program

---
+++ Fibbonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibbonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            for (int i = 0; i <=10 ;i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+            Console.ReadLine();
+        }     
+    }
+}
