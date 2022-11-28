bfc58d36308bfe1643a1d12d69ee61b86263bcb1
Nimap <Nimap@DESKTOP-22H8R70>
2022-11-28 15:47:40 +0530

    added reverse number

---
+++ Reverse Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int num = 154;
+            int num;
+            Console.WriteLine("Enter The Number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            int rem;
+
+            while(num!=0)
+            { 
+                rem = num % 10;
+                result = result * 10 + rem;
+                num = num / 10;
+            }
+            Console.WriteLine("The Reverse Number Is: " +result);
+            Console.ReadLine();
+        }
+    }
+}
